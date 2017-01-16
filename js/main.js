1c7c894ed7d3e3415305048b797093ea22e2940a
root <root@TLV-DEV-8201.bdb.io>
2017-01-16 12:12:52 +0200

    added language toggle, better styling

--- js/main.js
+++ js/main.js
@@ -1,77 +1,81 @@
 
-Vue.transition('mywords',{
-  enterClass: 'fadeInUp',
-  leaveClass: 'fadeOutDown'
+Vue.transition('mywords', {
+    enterClass: 'fadeInUp',
+    leaveClass: 'fadeOutDown'
 });
 
-Vue.transition('learnedwords',{
-  enterClass: 'fadeInDown',
-  leaveClass: 'fadeOutUp'
+Vue.transition('learnedwords', {
+    enterClass: 'fadeInDown',
+    leaveClass: 'fadeOutUp'
 });
 
-Vue.transition('hiddenwords',{
-  enterClass: 'fadeInDown',
-  leaveClass: 'fadeOutUp'
+Vue.transition('hiddenwords', {
+    enterClass: 'fadeInDown',
+    leaveClass: 'fadeOutUp'
 });
 
 
 
 new Vue({
-  el:'body',
-  data:{
-     newWord: '',
-     mywords: [
-      {trans: 'een', origin:'one'},
-      {trans: 'twee', origin: 'two'},
-      {trans: 'drie', origin: 'three'},
-      {trans: 'vier', origin: 'four'},
-      {trans: 'vijf', origin:'five'},
-      {trans: 'zes', origin: 'six'},
-      {trans: 'zeven', origin: 'seven'},
-      {trans: 'acht', origin: 'eight'},
-    ],
-    learnedwords: [
-       {trans: 'huis', origin: 'home'},
-    ],
-    hiddenwords: [
-       {trans: 'Olga', origin: 'olga'},
-    ],
-  }, 
-  methods:{
-    addNewWord: function(){
-      var newWordArr = this.newWord.split('-');
-      if(newWordArr.length ==2){
-        var newWordObj = {trans: newWordArr[0], origin: newWordArr[1]};
-        this.mywords.push(newWordObj);
-        this.newWord = '';
-      }else{
-        this.newWord = this.newWord + ' ' + 'ERROR';
-      }
-      
+    el: 'body',
+    data: {
+        showTrans: true,
+        newWord: '',
+        mywords: [
+            {trans: 'een', origin: 'one'},
+            {trans: 'twee', origin: 'two'},
+            {trans: 'drie', origin: 'three'},
+            {trans: 'vier', origin: 'four'},
+            {trans: 'vijf', origin: 'five'},
+            {trans: 'zes', origin: 'six'},
+            {trans: 'zeven', origin: 'seven'},
+            {trans: 'acht', origin: 'eight'},
+        ],
+        learnedwords: [
+            {trans: 'huis', origin: 'home'},
+        ],
+        hiddenwords: [
+            {trans: 'Olga', origin: 'olga'},
+        ],
     },
-    removeWord: function(item){   
-      this.mywords.$remove(item);
-      this.hiddenwords.push(item);
+    methods: {
+        addNewWord: function () {
+            var newWordArr = this.newWord.split('-');
+            if (newWordArr.length === 2) {
+                var newWordObj = {trans: newWordArr[0], origin: newWordArr[1]};
+                this.mywords.push(newWordObj);
+                this.newWord = '';
+            } else {
+                this.newWord = this.newWord + ' ' + 'ERROR';
+            }
+
+        },
+        removeWord: function (item) {
+            this.mywords.$remove(item);
+            this.hiddenwords.push(item);
+        },
+        markLearned: function (item) {
+            this.mywords.$remove(item);
+            this.learnedwords.push(item);
+        },
+        unHideAWord: function (item) {
+            this.hiddenwords.$remove(item);
+            this.mywords.push(item);
+        },
+        learnWordAgain: function (item) {
+            this.learnedwords.$remove(item);
+            this.mywords.push(item);
+        },
+        toggleLanguage: function () {
+            this.showTrans = !this.showTrans;
+        }
     },
-    markLearned: function(item){
-       this.mywords.$remove(item);
-       this.learnedwords.push(item);
-    },
-    unHideAWord: function(item){
-       this.hiddenwords.$remove(item);   
-       this.mywords.push(item);
-    },
-    learnWordAgain: function(item){    
-       this.learnedwords.$remove(item);
-       this.mywords.push(item);
-    }, 
-  },
-  watch: {
-    learnedwords: function () {
-       console.log(JSON.stringify(this.learnedwords))
-    },
-    hiddenwords: function () {
-      console.log(JSON.stringify(this.hiddenwords))
+    watch: {
+        learnedwords: function () {
+            console.log(JSON.stringify(this.learnedwords))
+        },
+        hiddenwords: function () {
+            console.log(JSON.stringify(this.hiddenwords))
+        }
     }
-  }
 });
