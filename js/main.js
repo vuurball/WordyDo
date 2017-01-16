1862d52792e8b03aadb545d25ee91c09e3f612f6
root <root@TLV-DEV-8201.bdb.io>
2017-01-16 15:58:55 +0200

    add new word, save lists, show translation functionality and better styling

--- js/main.js
+++ js/main.js
@@ -20,34 +20,51 @@ new Vue({
     el: 'body',
     data: {
         showTrans: true,
-        newWord: '',
+        newWordTrans: '',
+        newWordOrigin: '',
         mywords: [
-            {trans: 'een', origin: 'one'},
-            {trans: 'twee', origin: 'two'},
-            {trans: 'drie', origin: 'three'},
-            {trans: 'vier', origin: 'four'},
-            {trans: 'vijf', origin: 'five'},
-            {trans: 'zes', origin: 'six'},
-            {trans: 'zeven', origin: 'seven'},
-            {trans: 'acht', origin: 'eight'},
+//            {trans: 'een', origin: 'one'},
+//            {trans: 'twee', origin: 'two'},
+//            {trans: 'drie', origin: 'three'},
+//            {trans: 'vier', origin: 'four'},
+//            {trans: 'vijf', origin: 'five'},
+//            {trans: 'zes', origin: 'six'},
+//            {trans: 'zeven', origin: 'seven'},
+//            {trans: 'acht', origin: 'eight'},
         ],
         learnedwords: [
-            {trans: 'huis', origin: 'home'},
+//            {trans: 'huis', origin: 'home'},
         ],
         hiddenwords: [
-            {trans: 'Olga', origin: 'olga'},
-        ],
+            // {trans: 'Olga', origin: 'olga'},
+        ]
+    },
+    ready: function () {
+        //get the lists content from cookie
+        var listsArr = JSON.parse(this.getCookie('wordslist'));
+        //ini each list
+        this.mywords = listsArr['mywords'];
+        this.learnedwords = listsArr['learnedwords'];
+        this.hiddenwords = listsArr['hiddenwords'];
+        //add showHint=false property to each word in mywords list
+        this.mywords.forEach(function (obj) {
+            obj.showHint = false;
+        });
     },
     methods: {
         addNewWord: function () {
-            var newWordArr = this.newWord.split('-');
-            if (newWordArr.length === 2) {
-                var newWordObj = {trans: newWordArr[0], origin: newWordArr[1]};
-                this.mywords.push(newWordObj);
-                this.newWord = '';
-            } else {
-                this.newWord = this.newWord + ' ' + 'ERROR';
-            }
+            var newWordObj = {trans: this.newWordTrans, origin: this.newWordOrigin, showHint: false};
+            this.mywords.push(newWordObj);
+            this.newWordOrigin = this.newWordTrans = '';
+
+//            var newWordArr = this.newWord.split('-');
+//            if (newWordArr.length === 2) {
+//                var newWordObj = {trans: newWordArr[0], origin: newWordArr[1]};
+//                this.mywords.push(newWordObj);
+//                this.newWord = '';
+//            } else {
+//                this.newWord = this.newWord + ' ' + 'ERROR';
+//            }
 
         },
         removeWord: function (item) {
@@ -68,14 +85,51 @@ new Vue({
         },
         toggleLanguage: function () {
             this.showTrans = !this.showTrans;
+        },
+        toggleHint: function (item) {
+            item.showHint = !item.showHint;
+        },
+        setCookie: function (cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toUTCString();
+            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        },
+        checkCookie: function () {
+            var username = getCookie("username");
+            if (username != "") {
+                alert("Welcome again " + username);
+            } else {
+                username = prompt("Please enter your name:", "");
+                if (username != "" && username != null) {
+                    setCookie("username", username, 365);
+                }
+            }
+        },
+        getCookie: function (cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
         }
+
     },
     watch: {
-        learnedwords: function () {
-            console.log(JSON.stringify(this.learnedwords))
+        mywords: function () {
+            //remove showHint=false property from each word in mywords list before saving
+            this.mywords.forEach(function (obj) {
+                obj.showHint === undefined;
+            });
+            var listsArr = {'mywords': this.mywords, 'learnedwords': this.learnedwords, 'hiddenwords': this.hiddenwords};
+            this.setCookie('wordslist', JSON.stringify(listsArr), 5);
         },
-        hiddenwords: function () {
-            console.log(JSON.stringify(this.hiddenwords))
-        }
     }
 });
